6fcc4bbbcea881b6f5e6fefef2f4e3d7c8533890
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-08-23 19:34:26 +0530

    Time: 4 ms (84.03%), Space: 19.4 MB (97.04%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,25 +1,40 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         vector<int> temp;
+
+//         for(int i = 0; i < nums.size(); i++) {
+//             int count = 0;
+
+//             for(int j = 0; j < temp.size(); j++) {
+//                 if(temp[j] == nums[i])
+//                     count++;
+//             }
+
+//             if(count < 2) {
+//                 temp.push_back(nums[i]);
+//             }
+//         }
+
+//         for(int i = 0; i < temp.size(); i++) {
+//             nums[i] = temp[i];
+//         }
+
+//         return temp.size();
+//     }
+// };
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int> temp;
+        int k = 0;
 
         for(int i = 0; i < nums.size(); i++) {
-            int count = 0;
-
-            for(int j = 0; j < temp.size(); j++) {
-                if(temp[j] == nums[i])
-                    count++;
-            }
-
-            if(count < 2) {
-                temp.push_back(nums[i]);
+            if(k < 2 || nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
             }
         }
 
-        for(int i = 0; i < temp.size(); i++) {
-            nums[i] = temp[i];
-        }
-
-        return temp.size();
+        return k;
     }
 };
