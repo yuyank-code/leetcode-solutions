56c862475c75ea6291e58b1f13f1f6c03541427e
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-08-25 18:44:38 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,15 +1,46 @@
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n=nums.size();
+//      for(int i=0;i<n-1;i++){
+//         for(int j=0;j<n-i-1;j++){
+//             if(nums[j]>nums[j+1]){
+//                 int temp=nums[j+1];
+//                 nums[j+1]=nums[j];
+//                 nums[j]=temp;            }
+//         }
+//      }
+//     // sort(nums.begin(),nums.end());
+//     }
+// };
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
-     for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(nums[j]>nums[j+1]){
-                int temp=nums[j+1];
-                nums[j+1]=nums[j];
-                nums[j]=temp;            }
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int x : nums) {
+            if (x == 0)
+                count0++;
+            else if (x == 1)
+                count1++;
+            else
+                count2++;
         }
-     }
-    // sort(nums.begin(),nums.end());
+
+        int i = 0;
+
+        while (count0--) {
+            nums[i++] = 0;
+        }
+
+        while (count1--) {
+            nums[i++] = 1;
+        }
+
+        while (count2--) {
+            nums[i++] = 2;
+        }
     }
 };
