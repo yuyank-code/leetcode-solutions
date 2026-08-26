6f1803c3ceaca0cc5aa003f6f3ed30c7bb315063
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-08-26 19:33:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (33.94%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,15 +1,36 @@
-class Solution {
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         map<int,int>mpp;
+//         for(int i=0;i<numbers.size();i++){
+//             int a=numbers[i];
+//             int more=target-a;
+//             if(mpp.find(more)!=mpp.end()){
+//                 return {mpp[more],i+1};
+//             }
+//             mpp[numbers[i]]=i+1;
+//         }
+//         return{};
+//     }
+// };
+
+class Solution{
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        map<int,int>mpp;
-        for(int i=0;i<numbers.size();i++){
-            int a=numbers[i];
-            int more=target-a;
-            if(mpp.find(more)!=mpp.end()){
-                return {mpp[more],i+1};
-            }
-            mpp[numbers[i]]=i+1;
-        }
-        return{};
+ vector<int> twoSum(vector<int>& num, int target) {
+int left=0;
+int right=num.size()-1;
+while(left<right){
+    int sum=num[left]+num[right];
+    if(sum==target){
+        return{left+1,right+1};
     }
+    if(sum<target){
+        left++;
+    }
+    else{
+        right--;
+    }
+}
+return{};
+}
 };
