db72346df9f37d8113b73f03e3b5839b4c95020e
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-08-27 10:27:16 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (14.79%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,10 +1,24 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//             unordered_map<int,int>temp;
+//              for(int i=0;i<nums.size();i++){
+//                 temp[nums[i]]+=1;
+//                 if(temp[nums[i]] > nums.size() / 2) {
+//                 return nums[i];
+//             }
+//             }
+//         return -1;
+//     }
+// };
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-            unordered_map<int,int>temp;
-             for(int i=0;i<nums.size();i++){
+        int n=nums.size();
+            map<int,int>temp;
+             for(int i=0;i<n;i++){
                 temp[nums[i]]+=1;
-                if(temp[nums[i]] > nums.size() / 2) {
+                if(temp[nums[i]] > n / 2) {
                 return nums[i];
             }
             }
