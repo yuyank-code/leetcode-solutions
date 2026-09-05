921e9788965f7f2608a31f5f0cbae369b220480d
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-09-05 17:38:15 +0530

    Time: 3 ms (95.01%) | Memory: 202.6 MB (85.02%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -26,3 +26,25 @@ public:
         return -1;
     }
 };
+// class Solution {
+// public:
+//     int firstStableIndex(vector<int>& nums, int k) {
+//         int n = nums.size();
+
+//         int maxi = nums[0];
+
+//         for(int i = 0; i < n; i++) {
+//             maxi = max(maxi, nums[i]);
+
+//             int mini = *min_element(nums.begin() + i, nums.end());
+
+//             int ans = maxi - mini;
+
+//             if(ans <= k) {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
