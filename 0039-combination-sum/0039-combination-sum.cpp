aad04ea5ecaff7ec22bd5ebe8d4bea06f0819995
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-09-07 15:27:04 +0530

    Time: 3 ms (51.4%), Space: 14.3 MB (39.05%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,36 +1,59 @@
-class Solution {
-public:
-    vector<vector<int>> ans;
-    vector<int> current;
+// class Solution {
+// public:
+//     vector<vector<int>> ans;
+//     vector<int> current;
 
-    void solve(vector<int>& candidates, int remaining, int start) {
+//     void solve(vector<int>& candidates, int remaining, int start) {
 
-        // Target achieved
-        if (remaining == 0) {
+//         // Target achieved
+//         if (remaining == 0) {
+//             ans.push_back(current);
+//             return;
+//         }
+
+//         // We have gone beyond the target
+//         if (remaining < 0) {
+//             return;
+//         }
+
+//         for (int i = start; i < candidates.size(); i++) {
+
+//             // Temporarily select this number
+//             current.push_back(candidates[i]);
+
+//             // Subtract it from the remaining target
+//             solve(candidates, remaining - candidates[i], i);
+
+//             // Remove it and try the next number
+//             current.pop_back();
+//         }
+//     }
+
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         solve(candidates, target, 0);
+//         return ans;
+//     }
+// };
+class Solution{
+    public:
+    vector<vector<int>>ans;
+    vector<int>current;
+    void solve(vector<int>&candidates,int remaining,int start){
+        if(remaining==0){
             ans.push_back(current);
             return;
         }
-
-        // We have gone beyond the target
-        if (remaining < 0) {
+        if(remaining<0){
             return;
         }
-
-        for (int i = start; i < candidates.size(); i++) {
-
-            // Temporarily select this number
+        for(int i=start;i<candidates.size();i++){
             current.push_back(candidates[i]);
-
-            // Subtract it from the remaining target
-            solve(candidates, remaining - candidates[i], i);
-
-            // Remove it and try the next number
+            solve(candidates,remaining-candidates[i],i);
             current.pop_back();
         }
     }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        solve(candidates, target, 0);
+    vector<vector<int>> combinationSum(vector<int>& candidates,int target){
+        solve(candidates,target,0);
         return ans;
     }
 };
