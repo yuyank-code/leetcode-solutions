10855475537381880df523d2622a43b3c2242939
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-09-08 17:04:03 +0530

    Time: 0 ms (100%), Space: 7.6 MB (88.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -7,17 +7,32 @@
 //         return fib(n-1)+fib(n-2);
 //     }
 // };
+// class Solution{
+//     public:
+//     int fib(int n){
+//         vector<int>dp(n+1,-1);
+//         return solve(n,dp);
+//     }
+//     int solve(int n,vector<int>&dp){
+//         if(n<=1)
+//             return n;
+//         if(dp[n]!= -1)
+//             return dp[n];
+//         return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+//     }
+// };
 class Solution{
-    public:
+public:
     int fib(int n){
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
-    }
-    int solve(int n,vector<int>&dp){
-        if(n<=1)
-            return n;
-        if(dp[n]!= -1)
-            return dp[n];
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        if(n == 0)
+            return 0;
+        int prev2=0;
+        int prev1=1;
+        for(int i=2;i<=n;i++){
+            int curi=prev1+prev2;
+            prev2=prev1;
+            prev1=curi;
+        }
+        return prev1;
     }
 };
