5d671c8b1fe553cb2780207babc87fb3aa522962
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-09-08 16:41:58 +0530

    Time: 3 ms (64.48%) | Memory: 8.4 MB (11.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,23 @@
-class Solution {
-public:
-    int fib(int n) {
-        if(n<=1){
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n<=1){
+//             return n;
+//         }
+//         return fib(n-1)+fib(n-2);
+//     }
+// };
+class Solution{
+    public:
+    int fib(int n){
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
+    int solve(int n,vector<int>&dp){
+        if(n<=1)
             return n;
-        }
-        return fib(n-1)+fib(n-2);
+        if(dp[n]!= -1)
+            return dp[n];
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
     }
 };
