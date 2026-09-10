1b23d3a9187d72e11a4ddccf66da917f53a6ee53
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-09-10 17:26:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (49.40%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,16 +1,30 @@
+// class Solution {
+// public:
+//     int countCommas(int n) {
+//         int count = 0;
+
+//         if (n >= 1000)
+//             count += n - 999;
+
+//         if (n >= 1000000)
+//             count += n - 999999;
+
+//         if (n >= 1000000000)
+//             count += n - 999999999;
+
+//         return count;
+//     }
+// };
 class Solution {
 public:
     int countCommas(int n) {
         int count = 0;
+        int x = 1000;
 
-        if (n >= 1000)
-            count += n - 999;
-
-        if (n >= 1000000)
-            count += n - 999999;
-
-        if (n >= 1000000000)
-            count += n - 999999999;
+        while (x <= n) {
+            count += n - x + 1;
+            x *= 1000;
+        }
 
         return count;
     }
