765b94053a8712d407991383492b6a9d3bfe835c
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-09-15 17:37:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (97.13%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,30 +1,51 @@
+// class Solution {
+// public:
+// void solve(int index,vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans){
+//     if(index==nums.size()){
+//         ans.push_back(ds);
+//         return;
+//     }
+//     ds.push_back(nums[index]);
+//     solve(index+1,nums,ds,ans);
+//     ds.pop_back();
+//     solve(index+1,nums,ds,ans);
+// }
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         vector<vector<int>>ans;
+//         vector<int>ds;
+//         set<vector<int>> result;
+//         solve(0,nums,ds,ans);
+//         for(auto x:ans){
+//             result.insert(x);
+//         }
+//         vector<vector<int>> finalAns;
+
+//         for(auto x : result) {
+//             finalAns.push_back(x);
+//         }
+
+//         return finalAns;
+//     }
+// };
 class Solution {
 public:
 void solve(int index,vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans){
-    if(index==nums.size()){
-        ans.push_back(ds);
-        return;
+    ans.push_back(ds);
+    for(int i=index;i<nums.size();i++){
+        if(i!=index&&nums[i]==nums[i-1]) continue;
+        ds.push_back(nums[i]);
+        solve(i+1,nums,ds,ans);
+        ds.pop_back();
     }
-    ds.push_back(nums[index]);
-    solve(index+1,nums,ds,ans);
-    ds.pop_back();
-    solve(index+1,nums,ds,ans);
+
 }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         vector<int>ds;
-        set<vector<int>> result;
+        sort(nums.begin(),nums.end());
         solve(0,nums,ds,ans);
-        for(auto x:ans){
-            result.insert(x);
-        }
-        vector<vector<int>> finalAns;
-
-        for(auto x : result) {
-            finalAns.push_back(x);
-        }
-
-        return finalAns;
+        return ans;
+    
     }
 };
