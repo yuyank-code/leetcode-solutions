1b84826877cbb6047576eaf612d573d5f640fe1b
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-09-18 18:51:48 +0530

    Time: 303 ms (5.37%) | Memory: 23 MB (98.89%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,44 +1,90 @@
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+//         // Temporary vector: har string ka sorted version
+//         vector<string> neww;
+
+//         for (auto x : strs) {
+//             sort(x.begin(), x.end());
+//             neww.push_back(x);
+//         }
+
+//         // Kaunsa index already kisi group mein use ho chuka hai
+//         vector<int> used(strs.size(), 0);
+
+//         vector<vector<string>> ans;
+
+//         for (int i = 0; i < neww.size(); i++) {
+
+//             // Agar already kisi group mein aa chuka hai
+//             if (used[i])
+//                 continue;
+
+//             vector<string> temp;
+
+//             // Same sorted string wale indexes dhoondo
+//             for (int j = i; j < neww.size(); j++) {
+
+//                 if (neww[i] == neww[j]) {
+
+//                     // Original string uthao
+//                     temp.push_back(strs[j]);
+
+//                     // Is index ko processed mark karo
+//                     used[j] = 1;
+//                 }
+//             }
+
+//             ans.push_back(temp);
+//         }
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        // Temporary vector: har string ka sorted version
-        vector<string> neww;
+        int n = strs.size();
 
-        for (auto x : strs) {
-            sort(x.begin(), x.end());
-            neww.push_back(x);
+        // Har string ka sorted version
+        vector<string> sorted_word(n);
+
+        for (int i = 0; i < n; i++) {
+            sorted_word[i] = strs[i];
+            sort(sorted_word[i].begin(), sorted_word[i].end());
         }
 
-        // Kaunsa index already kisi group mein use ho chuka hai
-        vector<int> used(strs.size(), 0);
+        vector<vector<string>> groups;
 
-        vector<vector<string>> ans;
+        // Track ki kaunsa index already kisi group mein use ho chuka hai
+        vector<bool> used(n, false);
 
-        for (int i = 0; i < neww.size(); i++) {
+        for (int i = 0; i < n; i++) {
 
-            // Agar already kisi group mein aa chuka hai
-            if (used[i])
+            if (used[i]) {
                 continue;
+            }
 
-            vector<string> temp;
+            vector<string> group;
 
-            // Same sorted string wale indexes dhoondo
-            for (int j = i; j < neww.size(); j++) {
+            // Current word ko group mein daalo
+            group.push_back(strs[i]);
+            used[i] = true;
 
-                if (neww[i] == neww[j]) {
+            // Baaki words check karo
+            for (int j = i + 1; j < n; j++) {
 
-                    // Original string uthao
-                    temp.push_back(strs[j]);
-
-                    // Is index ko processed mark karo
-                    used[j] = 1;
+                if (!used[j] && sorted_word[i] == sorted_word[j]) {
+                    group.push_back(strs[j]);
+                    used[j] = true;
                 }
             }
 
-            ans.push_back(temp);
+            groups.push_back(group);
         }
 
-        return ans;
+        return groups;
     }
 };
