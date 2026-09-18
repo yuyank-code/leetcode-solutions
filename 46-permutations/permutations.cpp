98f5cf67439fbda15ec413f68bedc2c472f2f3db
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-09-18 17:50:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (40.12%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,41 +1,75 @@
+// class Solution {
+// public:
+//     vector<vector<int>> ans;
+    
+//     void solve(vector<int>& nums, vector<int>& temp, vector<int>& used) {
+        
+//         // permutation complete
+//         if(temp.size() == nums.size()) {
+//             ans.push_back(temp);
+//             return;
+//         }
+
+//         // current position ke liye element choose karo
+//         for(int i = 0; i < nums.size(); i++) {
+            
+//             // already use ho chuka hai
+//             if(used[i] == 1)
+//                 continue;
+
+//             // pick
+//             used[i] = 1;
+//             temp.push_back(nums[i]);
+
+//             solve(nums, temp, used);
+
+//             // backtrack
+//             temp.pop_back();
+//             used[i] = 0;
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int>& nums) {
+        
+//         vector<int> temp;
+//         vector<int> used(nums.size(), 0);
+
+//         solve(nums, temp, used);
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
-    vector<vector<int>> ans;
+  
     
-    void solve(vector<int>& nums, vector<int>& temp, vector<int>& used) {
-        
-        // permutation complete
-        if(temp.size() == nums.size()) {
-            ans.push_back(temp);
+    void solve(vector<int>&ds,vector<int>& nums, vector<vector<int>>& ans, int freq[]) {
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
             return;
         }
-
-        // current position ke liye element choose karo
-        for(int i = 0; i < nums.size(); i++) {
-            
-            // already use ho chuka hai
-            if(used[i] == 1)
-                continue;
-
-            // pick
-            used[i] = 1;
-            temp.push_back(nums[i]);
-
-            solve(nums, temp, used);
-
-            // backtrack
-            temp.pop_back();
-            used[i] = 0;
+        for(int i=0;i<nums.size();i++){
+            if(!freq[i]){
+                ds.push_back(nums[i]);
+                freq[i]=1;
+                solve(ds,nums,ans,freq) ;
+                freq[i]=0;
+                ds.pop_back();         
+                }
         }
+        
+
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
-        
-        vector<int> temp;
-        vector<int> used(nums.size(), 0);
-
-        solve(nums, temp, used);
-
+        vector<vector<int>>ans;
+        vector<int>ds;
+        int freq[nums.size()];
+        for(int i=0;i<nums.size();i++) freq[i]=0;
+        solve(ds,nums,ans,freq);
         return ans;
+
+
+      
     }
 };
