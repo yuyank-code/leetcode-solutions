845307c90c1b95ea2c633fe9714f076cbef29108
Yuyank Singh <yuyanksingh9455@gmail.com>
2026-09-21 17:50:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (82.64%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,26 +1,45 @@
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
+        
+//         int n = nums.size();
+       
+//         int i = n - 2;
+        
+//         while (i >= 0 && nums[i] >= nums[i + 1]) {
+//             i--;
+//         }
+        
+//         if (i >= 0) {
+            
+//             int j = n - 1;
+            
+//             while (nums[j] <= nums[i]) {
+//                 j--;
+//             }
+//             swap(nums[i], nums[j]);
+//         }
+        
+
+//         reverse(nums.begin() + i + 1, nums.end());
+//     }
+// };
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        
-        int n = nums.size();
-       
-        int i = n - 2;
-        
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-        
-        if (i >= 0) {
-            
-            int j = n - 1;
-            
-            while (nums[j] <= nums[i]) {
-                j--;
+        int n = nums.size(), index = -1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
             }
-            swap(nums[i], nums[j]);
         }
-        
-
-        reverse(nums.begin() + i + 1, nums.end());
+        for(int i=n-1; i>=index && index != -1; i--){
+            if(nums[i] > nums[index]){
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin() + index + 1, nums.end());
     }
 };
